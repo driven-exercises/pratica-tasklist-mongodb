1cd6da8ca2a2835bad8562c33d099bef0e4713ae
Frank Rocha <contato.frankrocha@gmail.com>
2023-04-13 12:20:58 -0300

    Ajustes nos testes

--- .hub/tests/app.test.js
+++ .hub/tests/app.test.js
@@ -2,8 +2,9 @@ import request from "../utils/request";
 import { _setup, end, setEnv } from "../utils/serverRunner";
 import sleep from "../utils/sleep";
 import fs from "fs";
+import { jest } from '@jest/globals'
 
-jest.setTimeout(15000);
+// jest.setTimeout(25000);
 
 beforeAll(async () => {
   setEnv("MONGO_URI", "mongodb://mongo_db:27017");
@@ -14,72 +15,81 @@ afterAll(() => {
   end();
 });
 
-describe("GET /tasks", () => {
-  it("should respond with an empty array", async () => {
-    await _setup();
+// describe("GET /tasks", () => {
+//   it("should respond with an empty array", async () => {
+//     await _setup();
 
-    const response = await request.get("/tasks");
+//     const response = await request.get("/tasks");
 
-    expect(response.data).toEqual([]);
-  });
+//     expect(response.data).toEqual([]);
+//   });
 
-  it("should responde with status 404", async () => {
-    await _setup();
+//   it("should responde with status 404", async () => {
+//     await _setup();
 
-    const nonExistingId = "non-existing";
+//     const nonExistingId = "non-existing";
 
-    const response = await request.get(`/tasks/${nonExistingId}`);
+//     const response = await request.get(`/tasks/${nonExistingId}`);
 
-    expect(response.status).toBe(404);
-  });
+//     expect(response.status).toBe(404);
+//   });
 
-  it("should create a new task", async () => {
-    await _setup();
+//   it("should create a new task", async () => {
+//     await _setup();
 
-    const response = await request.post("/tasks", {
-      titulo: "Fazer exercicio compilado",
-      descricao: "Fazer exercicio número 20 do compilado de back-end"
-    });
+//     const response = await request.post("/tasks", {
+//       titulo: "Fazer exercicio compilado",
+//       descricao: "Fazer exercicio número 20 do compilado de back-end"
+//     });
 
-    expect(response.status).toEqual(201);
-  });
+//     expect(response.status).toEqual(201);
+//   });
 
-  it("should respond with the complete list of tasks", async () => {
-    await _setup();
+//   it("should respond with the complete list of tasks", async () => {
+//     await _setup();
 
-    const response = await request.get("/tasks");
+//     const response = await request.get("/tasks");
 
-    expect(response.data).toEqual([{
-      _id: expect.any(String),
-      titulo: "Fazer exercicio compilado",
-      descricao: "Fazer exercicio número 20 do compilado de back-end"
-    }]);
-  });
+//     expect(response.data).toEqual([{
+//       _id: expect.any(String),
+//       titulo: "Fazer exercicio compilado",
+//       descricao: "Fazer exercicio número 20 do compilado de back-end"
+//     }]);
+//   });
 
-  it("should respond with given tasks", async () => {
-    await _setup();
+//   it("should respond with given tasks", async () => {
+//     await _setup();
 
-    const customerId = (await request.get("/tasks")).data[0]._id;
-    const response = await request.get(`/tasks/${customerId}`);
+//     const customerId = (await request.get("/tasks")).data[0]._id;
+//     const response = await request.get(`/tasks/${customerId}`);
 
-    expect(response.data).toEqual({
-      _id: customerId,
-      titulo: "Fazer exercicio compilado",
-      descricao: "Fazer exercicio número 20 do compilado de back-end"
-    });
-  });
-});
+//     expect(response.data).toEqual({
+//       _id: customerId,
+//       titulo: "Fazer exercicio compilado",
+//       descricao: "Fazer exercicio número 20 do compilado de back-end"
+//     });
+//   });
+// });
 
 describe("Usage of Async/Await", () => {
-  it("should be used async/await with try/catch instead of .then() and .catch()", async () => {
+  it.only("should be used async/await instead of .then() and .catch()", async () => {
     const code = fs.readFileSync("./src/app.js", "utf8");
 
     expect(code.indexOf("async")).toBeGreaterThan(-1);
     expect(code.indexOf("await")).toBeGreaterThan(-1);
+
+    expect(code.indexOf(".then(tasks")).toBe(-1);
+    expect(code.indexOf(".catch(")).toBe(-1);
+  });
+});
+
+describe("Usage of Try/Catch", () => {
+  it.only("should be used try/catch", async () => {
+    const code = fs.readFileSync("./src/app.js", "utf8");
+
     expect(code.indexOf("try")).toBeGreaterThan(-1);
     expect(code.indexOf("catch")).toBeGreaterThan(-1);
 
-    expect(code.indexOf(".then(product")).toBe(-1);
     expect(code.indexOf(".catch(")).toBe(-1);
   });
 });
